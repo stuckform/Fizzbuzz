7aa15fa497e04b6913f2f0996f1d3aed49fefed7
stuckform <75707524+stuckform@users.noreply.github.com>
2021-01-15 14:37:36 -0500

    commit

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -6,25 +6,19 @@ document.getElementById("fizzButton").addEventListener("click", function () {
 
 });
 
-  function fizzBuzz(num1, num2)
-  {
-    let returnValue = "";
-  for (let i = 1; i <= 100; i++)
-  {
-    if (i % num1 == 0 && i % num2 == 0)
-    {
-       document.getElementById("result").innerText += 'FizzBuzz ';      
-        } 
-    else if (i % num1 == 0)
-    {
-         document.getElementById("result").innerText += 'Fizz';
-        }
-        else if (i % num2 == 0) {
-           document.getElementById("result").innerText += 'Buzz';
-        }
-        else {
-          document.getElementById("result").innerText += i + ' ';
-        }
+function fizzBuzz(num1, num2) {
+  let results = "";
+  for (let i = 1; i <= 100; i++) {
+    if (i % num1 == 0 && i % num2 == 0) {
+      results += 'FizzBuzz ';
     }
-    return returnValue;
+    else if (i % num1 == 0) {
+      results += 'Fizz ';
+    }
+    else if (i % num2 == 0) {
+      results += 'Buzz ';
+    };
+     results += i + '  |';
+  }
+  document.getElementById("results").innerHTML= `${results}`
 };
